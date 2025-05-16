dde2ed73928338b3582236a73b7a49a7b94a92cd
Debora-Carvalho <deboravolc2@gmail.com>
2025-05-15 23:43:35 -0300

    adicionar outras imagens

--- src/components/ImagemCard/ImagemCard.jsx
+++ src/components/ImagemCard/ImagemCard.jsx
@@ -6,10 +6,12 @@ function ImagemCard({ imagem, aoClicar }) {
     <div className="cartao-imagem" onClick={() => aoClicar(imagem)}>
       <div className="imagem-wrapper">
         <img src={imagem.url} alt={imagem.titulo} />
+
         <div className="legenda">
           <h3>{imagem.titulo}</h3>
           <p>{imagem.largura}x{imagem.altura}px</p>
         </div>
+        
       </div>
     </div>
   );
